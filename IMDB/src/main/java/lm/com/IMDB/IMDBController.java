64d9d426c89f01673edca2462c8e45236de81012
Yount <N0099324@lm.lmig.com>
2017-06-27 11:56:42 -0400

    add person get

--- IMDB/src/main/java/lm/com/IMDB/IMDBController.java
+++ IMDB/src/main/java/lm/com/IMDB/IMDBController.java
@@ -10,9 +10,15 @@ public class IMDBController {
 
 	@RequestMapping(path = "/movie", method = RequestMethod.GET)
 	public String movie(Model model, String title, String genre, int year, String desc) {
-		System.out.println("here");
 		Movie m = new Movie(title, genre, year, desc);
 		model.addAttribute("movie", m);
 		return "movie";
+	}
+	
+	@RequestMapping(path = "/person", method = RequestMethod.GET)
+	public String person(Model model, String name, String gender, String dob, String type) {
+		Person p = new Person(name, gender, dob, type);
+		model.addAttribute("person", p);
+		return "person";
 	}
 }
